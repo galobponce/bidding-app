353064a42ccd8da6ed3f77248e13ba65e9a14da7
galobponce <personal.galo.bello@gmail.com>
2022-11-16 18:55:30 -0300

    added chakra config and provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { theme } from "./chakra";
+import GlobalRouter from './GlobalRouter';
+
+import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <GlobalRouter />
+    </ChakraProvider>  
   </React.StrictMode>
-)
+);

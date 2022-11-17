4416032d5a44cb5acec136070e0421df255374f6
galobponce <personal.galo.bello@gmail.com>
2022-11-17 00:59:36 -0300

    add redux provider in app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { theme } from "./chakra";
+import { globalStore } from './store';
 import GlobalRouter from './GlobalRouter';
+import { Toast } from './common/components';
 
 import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <GlobalRouter />
-    </ChakraProvider>  
+    <Provider store={globalStore}>
+      <ChakraProvider theme={theme}>
+        <Toast />
+        <GlobalRouter />
+      </ChakraProvider>  
+    </Provider>
   </React.StrictMode>
 );

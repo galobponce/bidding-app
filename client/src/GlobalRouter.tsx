f18725a7c8ff8dbd661809f566c840e2a085a9b0
galobponce <personal.galo.bello@gmail.com>
2022-11-18 00:39:08 -0300

    persists user when logged in before

--- client/src/GlobalRouter.tsx
+++ client/src/GlobalRouter.tsx
@@ -1,16 +1,42 @@
+import { Center } from '@chakra-ui/react';
 import { FC } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import { useAuthCheck } from './hooks';
+import { AppRouter } from './app/router';
 import { AuthRouter } from "./auth/router";
+import { CircleLoader } from './common/components';
 
 
+// Used to manage the routes of whole app
 const GlobalRouter: FC = () => {
 
+
+  const { status } = useAuthCheck();
+
+  // Shows loader when checking auth
+  if (status === 'checking') {
+    return (
+      <Center h='100vh'>
+        <CircleLoader />
+      </Center>
+    );
+  }
+
+  
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route path='/auth/*' element={<AuthRouter />} />
+        { 
+          // By this way, app routes are visible only if user is logged
+          // and auth routes are visible only if user is not logged
+          status === 'logged'
+          ?
+          <Route path='/*' element={<AppRouter />} />
+          :
+          <Route path='/auth/*' element={<AuthRouter />} />
+        }
 
         <Route path='/*' element={<Navigate to={'/auth/login'} />} />
         

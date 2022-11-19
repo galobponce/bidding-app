9f10267bfca74228c6dd255ec89ac8eede73fc7c
galobponce <personal.galo.bello@gmail.com>
2022-11-19 05:03:57 -0300

    nullable fields

--- server/items/models.py
+++ server/items/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Item(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
-    last_bid_user = models.IntegerField()
-    last_bid_price = models.IntegerField()
+    last_bid_user = models.IntegerField(default=None, blank=True, null=True)
+    last_bid_price = models.IntegerField(default=None, blank=True, null=True)
     closes_at = models.DateTimeField()
 
     class Meta:

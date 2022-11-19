54003c6044bef136c5e7da400529a81bb381f4aa
galobponce <personal.galo.bello@gmail.com>
2022-11-19 16:22:08 -0300

    apply layout in home

--- client/src/app/pages/Home.tsx
+++ client/src/app/pages/Home.tsx
@@ -1,8 +1,25 @@
 import { FC } from 'react';
+import { useGlobalSelector } from '../../hooks';
+import { AppLayout } from '../layout';
 
 
 export const Home: FC = () => {
+
+  const { isAdmin } = useGlobalSelector(state => state.auth)
+
   return (
-    <h1>Home</h1>
+    <AppLayout>
+
+      {
+        // If user is admin, sees the items table with CRUD
+        // If not, sees the gallery view to bid for items
+        isAdmin
+          ?
+          <h1>Items table</h1>
+          :
+          <h1>Items Gallery</h1>
+      }
+
+    </AppLayout>
   );
 };

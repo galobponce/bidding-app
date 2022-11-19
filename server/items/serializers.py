f8163b3a067ad1fe49db3994dfb74ea64e393f53
galobponce <personal.galo.bello@gmail.com>
2022-11-19 16:03:20 -0300

    add validator for bidding

--- server/items/serializers.py
+++ server/items/serializers.py
@@ -9,3 +9,23 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ['id', 'title', 'description', 'last_bid_user', 'last_bid_price', 'closes_at']
+
+
+    def validate_last_bid_price(self, value):
+        """
+        Checks that the new bid price must be higher than the current one
+        """
+        if value <= self.instance.last_bid_price:
+            raise serializers.ValidationError('New bid price must be higher than current one', code='invalid')
+            
+        return value
+
+
+    def validate_last_bid_user(self, value):
+        """
+        Checks if the user bidding is winning the auction
+        """
+        if value == self.instance.last_bid_user:
+            raise serializers.ValidationError('You cant bid if you are winning the auction', code='invalid')
+            
+        return value

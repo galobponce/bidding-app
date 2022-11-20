a8d3049bccdbadb36d5ce34929319ff2ca8821f5
galobponce <personal.galo.bello@gmail.com>
2022-11-20 16:15:53 -0300

    make default value

--- client/src/app/components/filters/OrderingSelector.tsx
+++ client/src/app/components/filters/OrderingSelector.tsx
@@ -19,13 +19,13 @@ export const OrderingSelector: FC = () => {
 
   // https://stackoverflow.com/questions/54069253/the-usestate-set-method-is-not-reflecting-a-change-immediately
   useEffect(() => {
-    dispatch(setOrderingFilter({ ordering }));
+    if (ordering) dispatch(setOrderingFilter({ ordering }));
   }, [ordering])
 
 
   return (
     <Center gap='3' w='15rem'>
-      <Select placeholder='Sort By' name='ordering' value={ordering} onChange={onInputChange}>
+      <Select name='ordering' value={ordering} onChange={onInputChange}>
         <option value='closes_at'>Earlier Close Date</option>
         <option value='-closes_at'>Older Close Date</option>
         <option value='last_bid_price'>Lower Price</option>

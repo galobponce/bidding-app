2ef35c1043e34ca35ab0266f068be563a325c1a4
galobponce <personal.galo.bello@gmail.com>
2022-11-20 00:32:49 -0300

    changed folder name

---
+++ client/src/app/components/admin/SearchBar.tsx
@@ -0,0 +1,48 @@
+import { FC, KeyboardEvent } from 'react';
+import { CloseIcon, SearchIcon, SmallCloseIcon } from '@chakra-ui/icons';
+import { InputGroup, Input, InputRightElement, IconButton } from '@chakra-ui/react';
+
+import { FilterComponentIterface } from './FilterComponentInterface';
+
+
+export const SearchBar: FC<FilterComponentIterface> = ({ value, handleFiltering, onInputChange }) => {
+
+
+  // If user presses 'enter' key while typing on input, filters
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      handleFiltering();
+    }
+  }
+
+
+  return (
+    <InputGroup w={{ base: 'xs', md: 'md', lg: 'lg' }}>
+      <Input
+        type='text'
+        name='search'
+        borderRadius='2xl'
+        placeholder='Search by title or description'
+        value={value}
+        onKeyDown={handleKeyDown}
+        onChange={onInputChange}
+      />
+      <InputRightElement>
+      <IconButton
+          variant='unstyled'
+          colorScheme='gray'
+          aria-label='Clear'
+          icon={<SmallCloseIcon />}
+        />
+        <IconButton
+          borderRadius='2xl'
+          variant='solid'
+          colorScheme='teal'
+          aria-label='Search'
+          icon={<SearchIcon />}
+          onClick={handleFiltering}
+        />
+      </InputRightElement>
+    </InputGroup>
+  );
+}

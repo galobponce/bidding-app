722c70f988a0fca6d516fda3ccf7520520ccbd8d
galobponce <personal.galo.bello@gmail.com>
2022-11-20 18:47:21 -0300

    made price default 0

--- server/items/models.py
+++ server/items/models.py
@@ -5,7 +5,7 @@ class Item(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
     last_bid_user = models.IntegerField(default=None, blank=True, null=True)
-    last_bid_price = models.IntegerField(default=None, blank=True, null=True)
+    last_bid_price = models.IntegerField(default=0)
     closes_at = models.DateTimeField()
 
     class Meta:

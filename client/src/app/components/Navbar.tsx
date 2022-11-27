acc5058830a1a44355c12303be67d0388ed61861
galobponce <personal.galo.bello@gmail.com>
2022-11-27 18:21:13 -0300

    color mode toggler now at settings

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -1,38 +1,48 @@
 import { FC } from 'react';
 import { MdLogout } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
-import { SunIcon, MoonIcon } from '@chakra-ui/icons';
-import { Center, Text, useColorMode, HStack, IconButton, useColorModeValue, Tooltip } from '@chakra-ui/react';
+import { AiFillHome, AiOutlineHome } from 'react-icons/ai';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { IoSettings, IoSettingsOutline } from 'react-icons/io5';
+import { Center, Text, useColorMode, HStack, IconButton, Tooltip } from '@chakra-ui/react';
 
 import { appRoutes } from '../router';
 import { NewItemButton } from './itemDetail';
 import { startLogout } from '../../store/auth';
 import { useGlobalDispatch } from '../../hooks';
-import { changeHtmlBackgroundColor } from '../utils';
 
 
 export const Navbar: FC = () => {
 
   const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useGlobalDispatch()
-  const SwitchIcon = useColorModeValue(MoonIcon, SunIcon);
-  const { colorMode, toggleColorMode } = useColorMode();
-
-
-  const onClickToggleColorMode = () => {
-    changeHtmlBackgroundColor(colorMode);
-    toggleColorMode();
-  };
+  const { colorMode } = useColorMode();
+  const HomeIcon = location.pathname === appRoutes.HOME ? AiFillHome : AiOutlineHome;
+  const SettingsIcon = location.pathname === appRoutes.SETTINGS ? IoSettings : IoSettingsOutline;
 
 
   return (
-    <Center zIndex='sticky' position='sticky' top='0' w='100%' h='100%' bg='white' _dark={{ bg: 'gray.800' }} px={['3', '6']} py='1' borderBottom='2px' borderColor='transparent' boxShadow='lg' justifyContent='space-between'>
+    <Center
+      zIndex='sticky'
+      position='sticky'
+      top='0'
+      w='100%'
+      h='100%'
+      bg='white'
+      _dark={{ bg: 'gray.800' }}
+      px={['3', '6']}
+      py='1'
+      borderBottom='2px'
+      borderColor='transparent'
+      boxShadow='lg'
+      justifyContent='space-between'
+    >
       <Center>
         <Text
           fontWeight='semibold'
           color={colorMode === 'dark' ? 'teal.400' : 'teal.500'}
           textAlign='center'
-          fontSize='3xl'
+          fontSize='2xl'
           onClick={() => navigate(appRoutes.HOME)}
         >
           Bidding App
@@ -40,16 +50,25 @@ export const Navbar: FC = () => {
       </Center>
       <HStack spacing='2'>
         <NewItemButton />
-        <Tooltip label="Toggle Color Mode"  aria-label='color mode tooltip'>
+        <Tooltip label="Home" aria-label='home tooltip'>
           <IconButton
-            fontSize='xl'
-            aria-label='Toggle color mode'
+            fontSize='2xl'
+            aria-label='Home'
             variant='ghost'
-            onClick={onClickToggleColorMode}
-            icon={<SwitchIcon />}
+            onClick={() => navigate(appRoutes.HOME)}
+            icon={<HomeIcon />}
           />
         </Tooltip>
-        <Tooltip label="Log Out"  aria-label='log out tooltip'>
+        <Tooltip label="Settings" aria-label='settings tooltip'>
+          <IconButton
+            fontSize='2xl'
+            aria-label='Settings'
+            variant='ghost'
+            onClick={() => navigate(appRoutes.SETTINGS)}
+            icon={<SettingsIcon />}
+          />
+        </Tooltip>
+        <Tooltip label="Log Out" aria-label='log out tooltip'>
           <IconButton
             fontSize='xl'
             aria-label='Lot Out'
